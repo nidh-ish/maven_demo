7d6d6b965b933680cdc7455ac8799c5e8ee218e7
nidh-ish <nidhishbhimrajka1@gmail.com>
2023-03-21 11:40:09 +0530

    Added loop in the calculator application

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -26,32 +26,30 @@ public class Calculator {
 		Scanner scanner = new Scanner(System.in);
 		Calculator calc = new Calculator();
 
-		System.out.print("Enter 1 for Squareroot function, 2 for Factorial function, 3 for NaturalLogarithm function, 4 for Power function, or 0 to exit: ");
-		int choice = scanner.nextInt();
+		while(true) {
+			System.out.print("Enter 1 for Squareroot function, 2 for Factorial function, 3 for NaturalLogarithm function, 4 for Power function, or 0 to exit: ");
+			int choice = scanner.nextInt();
 
-		if(choice == 0) {
-			return;
-		}
-		else if(choice == 1) {
-			System.out.print("Enter an integer to calculate its square root: ");
-			int n = scanner.nextInt();
-			System.out.println(calc.squareRoot(n));
-		}
-		else if(choice == 2) {
-			System.out.print("Enter an integer to calculate its factorial: ");
-			int n = scanner.nextInt();
-			System.out.println(calc.factorial(n));
-		}
-		else if(choice == 3) {
-			System.out.print("Enter a number to calculate its natural logarithm: ");
-			double n = scanner.nextDouble();
-			System.out.println(calc.naturalLog(n));
-		}
-		else if(choice == 4) {
-			System.out.print("Enter two integers x and b to calculate x to the power b: ");
-			double x = scanner.nextDouble();
-			double b = scanner.nextDouble();
-			System.out.println(calc.power(x, b));
+			if (choice == 0) {
+				return;
+			} else if (choice == 1) {
+				System.out.print("Enter an integer to calculate its square root: ");
+				int n = scanner.nextInt();
+				System.out.println(calc.squareRoot(n));
+			} else if (choice == 2) {
+				System.out.print("Enter an integer to calculate its factorial: ");
+				int n = scanner.nextInt();
+				System.out.println(calc.factorial(n));
+			} else if (choice == 3) {
+				System.out.print("Enter a number to calculate its natural logarithm: ");
+				double n = scanner.nextDouble();
+				System.out.println(calc.naturalLog(n));
+			} else if (choice == 4) {
+				System.out.print("Enter two integers x and b to calculate x to the power b: ");
+				double x = scanner.nextDouble();
+				double b = scanner.nextDouble();
+				System.out.println(calc.power(x, b));
+			}
 		}
 	}
 }
