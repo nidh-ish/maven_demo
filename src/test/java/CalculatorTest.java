c8c27240e3c40c420f208a184594284129bc5e95
nidh-ish <nidhishbhimrajka1@gmail.com>
2023-03-21 23:27:35 +0530

    Added falsenegatives

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class CalculatorTest {
     Calculator calc = new Calculator();
@@ -10,5 +11,13 @@ public class CalculatorTest {
 	    assertEquals("Finding factorial of a number for True Positive", 120, calc.factorial(5));
 	    assertEquals("Finding natural logarithm of a number for True Positive", 2.30258509299, calc.naturalLog(10), 0.001);
 	    assertEquals("Finding given power of a number for True Positive", 25.0, calc.power(5, 2), 0.001);
+    }
+
+    @Test
+    public void testFalsePositive(){
+        assertNotEquals("Finding square root of a number for False Positive", 4.0, calc.squareRoot(25), 0.001);
+        assertNotEquals("Finding factorial of a number for False Positive", 121, calc.factorial(5));
+        assertNotEquals("Finding natural logarithm of a number for False Positive", 3.30258509299, calc.naturalLog(10), 0.001);
+        assertNotEquals("Finding given power of a number for False Positive", 22.0, calc.power(5, 2), 0.001);
     }
 }
